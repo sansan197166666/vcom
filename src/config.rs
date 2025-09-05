d5945bf852e57c8abf06fcad1d6fff6e731623a3
sansan197166666 <sansan197166666@gmail.com>
2025-09-05 23:28:10 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,10 +100,10 @@ const CHARS: &[char] = &[
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[154.82.66.205"];
 pub const RS_PUB_KEY: &str = "39iCJusN1Mh+H7OqXB8i10yeRlfG2ytJbtBN5y3CQhM=";
-pub const RENDEZVOUS_PORT: i32 = 11256;
-pub const RELAY_PORT: i32 = 11257;
-pub const WS_RENDEZVOUS_PORT: i32 =11258;
-pub const WS_RELAY_PORT: i32 = 11259;
+pub const RENDEZVOUS_PORT: i32 = 21256;
+pub const RELAY_PORT: i32 = 21257;
+pub const WS_RENDEZVOUS_PORT: i32 =21258;
+pub const WS_RELAY_PORT: i32 = 21259;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
@@ -195,7 +195,7 @@ pub struct Socks5Server {
     pub password: String,
 }
 
-// more variable configss
+// more variable configs
 #[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq)]
 pub struct Config2 {
     #[serde(default, deserialize_with = "deserialize_string")]
